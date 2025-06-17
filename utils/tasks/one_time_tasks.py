b71f60b7cf7752597df22a528788f40d8d8134d5
Siddhanth Bhimakari <siddhanth.n.b@gmail.com>
2025-06-17 13:32:30 +0100

    added table creation task

--- utils/tasks/one_time_tasks.py
+++ utils/tasks/one_time_tasks.py
@@ -1,8 +1,23 @@
-import os
 import json
 from invoke.tasks import task
 from config.logger import logger
 from utils.helper import store_spotify_track_in_db
+
+@task()
+def create_tables_in_db():
+    from db.models.albums import Album
+    from db.models.artists import Artist
+    from db.models.tracks import Track
+    from db.models.album_artists import AlbumArtist
+    from db.models.track_artists import TrackArtist
+    from db.models.listening_history import ListeningHistory
+    from db.models.sync_logs import SyncLog
+    import utils.constants as constants
+    from sqlalchemy import create_engine
+    from db.models.base_model import Base
+
+    engine = create_engine(constants.SUPABASE_DB_URL)
+    Base.metadata.create_all(engine)
 
 @task()
 def populate_db_with_historical_listening_data():
