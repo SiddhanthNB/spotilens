c5145c15f2ad7dfa0fd18ed2d4b9f0b88d8c09ec
Siddhanth Bhimakari <siddhanth.n.b@gmail.com>
2025-06-27 01:45:31 +0100

    moved migrations into db/migrations folder

--- db/migrations/alembic/env.py
+++ db/migrations/alembic/env.py
@@ -1,9 +1,15 @@
 from logging.config import fileConfig
+import sys
+import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+# Add project root to path
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
+
 from utils.constants import SUPABASE_DB_URL, PROJECT_NAME
 from db.models.base_model import Base
 import db.models
